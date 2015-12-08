f3d6f92d7cb802da27b6285130f438e7e39c15cc
Zuly Alcaraz <zulyalcaraz@gmail.com>
2015-12-08 15:48:22 -0500

    Carga de otro videos en la vista con el video y la lista de reproduccion

--- client/app/routines/detail-routine/detail-routine.controller.js
+++ client/app/routines/detail-routine/detail-routine.controller.js
@@ -3,8 +3,12 @@
 angular.module('homegymApp')
   .controller('RoutinesDetailCtrl', function ($scope, $rootScope, $state, localStorageService, UserSrv)  {
     $scope.routine = localStorageService.get('detail-routine-info');
-    $scope.routines = localStorageService.get('routine-info')
+    $scope.routines = localStorageService.get('routine-info');
     $scope.actualDate = new Date();
-    $scope.link = 'https://www.youtube.com/watch?v=diFjQVUL7wk';
-    console.log(' $scope.routines',  $scope.routines);
+
+    $scope.changeRoutine = function (newRoutine, week) {
+    	newRoutine.week = parseInt(week);
+    	localStorageService.set('detail-routine-info', newRoutine);
+    	$state.reload();
+    };
   });
